552d0f94b68f52b31922c6922baa1110854c4cb9
Oliver Kell <oliverk.kell@gmail.com>
2016-05-15 20:11:17 +0300

    Not finished stuff

---
+++ app/src/main/java/com/ttu/idk1606/cards/app/activity/SendActivity.java
@@ -0,0 +1,15 @@
+package com.ttu.idk1606.cards.app.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ttu.idk1606.cards.app.R;
+
+public class SendActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send);
+    }
+
+}
